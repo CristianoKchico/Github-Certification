426554fe4f8bb9c1e8f2002b8e9dbe95ee5b0448
Cristiano Santos <129262790+CristianoKchico@users.noreply.github.com>
2024-11-22 15:37:48 +0000

    Adicionando files do desafio - Utilizando as Ferramentas do Github para Solucionar Algoritmos em Python

---
+++ Desafio_algoritmos_python_github/concat_dados.py
@@ -0,0 +1,8 @@
+# Recebendo strings do usuário e concatenando-as.
+
+entrada1 = input("Digite a primeira string: ")
+entrada2 = input("Digite a segunda string: ")
+
+concatenado = entrada1 + entrada2
+
+print(concatenado)
