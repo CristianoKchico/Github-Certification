fb84f06f70cb47a94b42acd020742859c7935907
Cristiano Santos <129262790+CristianoKchico@users.noreply.github.com>
2024-11-26 16:19:06 +0000

    Atualizando o file repeat_txt.

--- Desafio_algoritmos_python_github/repeat_txt.py
+++ Desafio_algoritmos_python_github/repeat_txt.py
@@ -4,3 +4,4 @@ string = input("Digite a string a ser repetida: ")
 numero = int(input("Digite o número de veszes para repetir a string: "))
 
 print((string + ' ') * numero)
+print(f"Sua string '{string}' foi repetida {numero} vezes.")
